c98e7dc58c941b0de4a81597d1f01d7abdfc2853
Ahmed Mahrous <ahmed.mahrous@gomini-app.com>
2024-02-11 16:15:38 +0200

    update Login Screen

--- lib/login.dart
+++ lib/login.dart
@@ -1,125 +1,218 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
-  const Login({super.key});
+  //MARK: - Properties
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
 
+  //MARK: - LifeCycle
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black45,
       appBar: AppBar(
-        backgroundColor: Colors.black45,
+        backgroundColor: Colors.blue.shade600,
         title: const Text(
           'Login',
           style: TextStyle(
             fontWeight: FontWeight.bold,
-            color: Colors.white
+            color: Colors.white,
           ),
         ),
         centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const SizedBox(
-              height: 60,
-            ),
-            const Image(
-              width: 100,
-              height: 100,
-              image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png'),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: 'E-mail',
-                prefixIcon: const Icon(
-                  Icons.email,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(
+                  height: 60,
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: const BorderSide(
-                    color: Colors.white,
+                const Image(
+                  width: 60,
+                  height: 60,
+                  image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png'),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  cursorColor: Colors.white,
+                  style: const TextStyle(
+                    color: Colors.white
                   ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: const BorderSide(
-                    color: Colors.grey,
-                    width: 2.0,
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                    labelStyle: const TextStyle(
+                      color: Colors.grey
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.email,
+                      color: Colors.white,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: const BorderSide(
+                        color: Colors.white,
+                        width: 1.0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: const BorderSide(
+                        color: Colors.white,
+                        width: 1.0,
+                      ),
+                    ),
                   ),
-                ),
-              ),
 
-              onFieldSubmitted: (value) {
-                print(value);
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                prefixIcon: const Icon(
-                  Icons.lock,
+                  onFieldSubmitted: (value) {
+                    print(value);
+                  },
                 ),
-                suffixIcon: const Icon(
-                  Icons.remove_red_eye,
+                const SizedBox(
+                  height: 20,
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: const BorderSide(
-                    color: Colors.white,
+                TextFormField(
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: const TextStyle(
+                      color: Colors.grey
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.lock,
+                      color: Colors.white,
+                    ),
+                    suffixIcon: const Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.white,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: const BorderSide(
+                        color: Colors.white,
+                        width: 1.0,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: const BorderSide(
+                        color: Colors.white,
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
+
+                  onFieldSubmitted: (value) {
+                    print(value);
+                  },
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 40,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: MaterialButton(
+                    onPressed: onPressLogin,
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17
+                      ),
+                    ),
                   ),
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  borderSide: const BorderSide(
-                    color: Colors.grey,
-                    width: 2.0,
+                SizedBox(
+                  height: 40,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        onPressed: onPressCreateAccount,
+                        child: const Text(
+                          'Forgot Password ?',
+                          style: TextStyle(
+                              color: Colors.blue
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-
-              onFieldSubmitted: (value) {
-                print(value);
-              },
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            Container(
-              width: double.infinity,
-              height: 40,
-              clipBehavior: Clip.hardEdge,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-              child: MaterialButton(
-                onPressed: () {
-                  print('Login');
-                },
-                color: Colors.blue,
-                textColor: Colors.white,
-                child: const Text(
-                  'Login'
+                const SizedBox(
+                  height: 40,
                 ),
-              ),
-
+                SizedBox(
+                  height: 30,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        'Don\'t have an account ?',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal
+                        ),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal
+                          ),
+                        ),
+                        onPressed: onPressCreateAccount,
+                        child: const Text(
+                          'Create an account',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  //MARK: - Support Functions
+  void onPressLogin() {
+    print(emailController.text);
+    print(passwordController.text);
+  }
+  void onPressCreateAccount() {
+    print('Create Account Tapped!');
   }
 }
